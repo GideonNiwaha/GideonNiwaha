e8b829944af1c4848c66a2b33b34bbdf0977d374
GideonNiwaha <niwahagideon7@gmail.com>
2024-09-20 12:22:39 +0300

    Create ExerciseOne.java

---
+++ ExerciseOne.java
@@ -0,0 +1,26 @@
+public class MyClass {
+  public static void main(String args[]) {
+     int b=12;
+     int a=6;
+     //Addition
+     int sum = a+b;
+     
+     //Subtraction
+     int difference = a-b;
+     
+     //Multiplication
+     int product = a*b;
+     
+     //division
+     int quotient = a/b;
+     
+     //Modulus
+     int modulus =a%b;
+
+     System.out.println("Sum " + sum);
+     System.out.println("subtraction " + difference);
+     System.out.println("multiplication " + product);
+     System.out.println("division " + quotient);
+     System.out.println("modulus " + modulus);
+  }
+}
